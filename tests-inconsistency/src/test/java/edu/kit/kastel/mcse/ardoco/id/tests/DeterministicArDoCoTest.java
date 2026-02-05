423bf7d48a893ca15a064b374fd3d9903deb3396
Dominik Fuchß <dominik.fuchss@kit.edu>
2026-02-05 16:36:46 +0100

    Sync from monorepo - main

--- tests-inconsistency/src/test/java/edu/kit/kastel/mcse/ardoco/id/tests/DeterministicArDoCoTest.java
+++
@@ -1,9 +0,0 @@
-/* Licensed under MIT 2024-2026. */
-package edu.kit.kastel.mcse.ardoco.id.tests;
-
-import com.tngtech.archunit.junit.AnalyzeClasses;
-
-@AnalyzeClasses(packages = { "edu.kit.kastel.mcse.ardoco.core", "edu.kit.kastel.mcse.ardoco.tlr", "edu.kit.kastel.mcse.ardoco.id" })
-public class DeterministicArdocoTest extends edu.kit.kastel.mcse.ardoco.core.tests.architecture.DeterministicArdocoTest {
-    // Has to be executed in this module
-}
